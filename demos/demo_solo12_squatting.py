56ef1e559fe63ab933d274a169f2bb1ac7d00bee
majidkhadiv <majidkhadiv@gmail.com>
2021-03-01 11:46:45 +0100

    add squatting demo

---
+++ demos/demo_solo12_squatting.py
@@ -0,0 +1,141 @@
+"""Squatting motion for solo12
+
+License BSD-3-Clause
+Copyright (c) 2020, New York University and Max Planck Gesellschaft.
+
+Author: Majid Khadiv
+"""
+
+import argparse
+import numpy as np
+from mim_control.robot_impedance_controller import RobotImpedanceController
+from mim_control.robot_centroidal_controller import RobotCentroidalController
+from bullet_utils.env import BulletEnvWithGround
+from robot_properties_solo.solo12wrapper import Solo12Robot, Solo12Config
+from robot_properties_bolt.bolt_wrapper import BoltRobot, BoltConfig
+
+
+def demo(robot_name):
+
+    # Create a Pybullet simulation environment
+    env = BulletEnvWithGround()
+
+    # Create a robot instance in the simulator.
+    if robot_name == "solo":
+        robot = env.add_robot(Solo12Robot)
+        robot_config = Solo12Config()
+        mu = 0.2
+        kc = [200, 200, 200]
+        dc = [5, 5, 5]
+        kb = [200, 200, 200]
+        db = [1.0, 1.0, 1.0]
+        qp_penalty_lin = 3 * [1e6]
+        qp_penalty_ang = 3 * [1e6]
+    elif robot_name == "bolt":
+        robot = env.add_robot(BoltRobot)
+        robot_config = BoltConfig()
+        mu = 0.2
+        kc = [0, 0, 100]
+        dc = [0, 0, 10]
+        kb = [100, 100, 100]
+        db = [10.0, 10.0, 10.0]
+        qp_penalty_lin = [1, 1, 1e6]
+        qp_penalty_ang = [1e6, 1e6, 1]
+    else:
+        raise RuntimeError(
+            "Robot name [" + str(robot_name) + "] unknown. "
+            "Try 'solo' or 'bolt'"
+        )
+
+    # Initialize control
+    tau = np.zeros(robot.nb_dof)
+
+    # simulation time (ms)
+    T = 10000
+
+    # Reset the robot to some initial state.
+    q0 = np.matrix(robot_config.initial_configuration).T
+    q0[0] = 0.0
+    dq0 = np.matrix(robot_config.initial_velocity).T
+    robot.reset_state(q0, dq0)
+
+    # Desired initial center of mass position and velocity.
+    x_com = np.zeros((T,3))
+    xd_com = np.zeros((T,3))
+    x_com[0,:] = [0.0, 0.0, 0.18]
+    xd_com[0,:] = [0.0, 0.0, 0.0]
+
+    # Generate squatting motion
+    period = [1000, 1000, 1000] #ms
+    amplitude = [0.0, 0.0, 0.05] #m
+    for j in range(T):
+        for i in range(3):
+            x_com[j,i] = x_com[0,i] + amplitude[i] * np.sin(2 * np.pi * j / period[i])
+            xd_com[j,i] = amplitude[i] * np.cos(2 * np.pi * j / period[i])
+
+    # The base should be flat.
+    x_ori = [0.0, 0.0, 0.0, 1.0]
+    x_angvel = [0.0, 0.0, 0.0]
+    # Alle end-effectors are in contact.
+    cnt_array = robot.nb_ee * [1]
+
+    # Impedance controller gains
+    kp = robot.nb_ee * [0.0, 0.0, 0.0]  # Disable for now
+    kd = robot.nb_ee * [0.0, 0.0, 0.0]
+    x_des = robot.nb_ee * [0.0, 0.0, -q0[2].item()]  # Desired leg length
+    xd_des = robot.nb_ee * [0.0, 0.0, 0.0]
+
+    # config_file = "./solo_impedance.yaml"
+    config_file = robot_config.paths["imp_ctrl_yaml"]
+    robot_cent_ctrl = RobotCentroidalController(
+        robot_config,
+        mu=mu,
+        kc=kc,
+        dc=dc,
+        kb=kb,
+        db=db,
+        qp_penalty_lin=qp_penalty_lin,
+        qp_penalty_ang=qp_penalty_ang,
+    )
+    robot_leg_ctrl = RobotImpedanceController(robot, config_file)
+
+    # Run the simulator for the trajectory
+    for i in range(T):
+        # Step the simulator.
+        env.step(
+            sleep=True
+        )  # You can sleep here if you want to slow down the replay
+        # Read the final state and forces after the stepping.
+        q, dq = robot.get_state()
+        # computing forces to be applied in the centroidal space
+        w_com = robot_cent_ctrl.compute_com_wrench(
+            q, dq, x_com[i,:], xd_com[i,:], x_ori, x_angvel
+        )
+        # distributing forces to the active end effectors
+        F = robot_cent_ctrl.compute_force_qp(q, dq, cnt_array, w_com)
+        # passing forces to the impedance controller
+        tau = robot_leg_ctrl.return_joint_torques(
+            q, dq, kp, kd, x_des, xd_des, F
+        )
+        # passing torques to the robot
+        robot.send_joint_command(tau)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--solo", help="Demonstrate Solo.", action="store_true"
+    )
+    parser.add_argument(
+        "--bolt", help="Demonstrate Bolt.", action="store_true"
+    )
+    args = parser.parse_args()
+    if args.solo:
+        robot_name = "solo"
+    elif args.bolt:
+        robot_name = "bolt"
+    else:
+        robot_name = "solo"
+
+    demo(robot_name)
