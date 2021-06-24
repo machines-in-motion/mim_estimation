269a53f94457e1540879f6d458ca3949b37be596
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-06-24 16:20:21 +0200

    some doc

--- include/mim_estimation/filtering_tools/ekf.hpp
+++ include/mim_estimation/filtering_tools/ekf.hpp
@@ -43,14 +43,15 @@
  *  The first equation depicts the "process" in the code.
  *  The second equation represents the "measurement" in the code.
  *
- *  Using a taylor expansion of the previous equaiton we get:
+ *  Using a taylor expansion of the previous equation we get:
  *  \f{align}
  *    x_t = g(u_t, \mu_{t-1}) + G_t \; * \; (x_{t-1} - \mu_{t-1}) + \epsilon_t \\
  *    z_t = h(\mu_t) + H_t \; * \; (x_{t} - \mu_{t}) + \delta_t
  *  \f}
  *  With \f$ \mu_{t} \f$ the average of the estimated state of covariance
  *  \f$ \Sigma_t \f$.
- *  The equation of the EKF routine are defined with the "update_filter" fonction.
+ *  The equation of the EKF routine are defined with the "update_filter"
+ *  function.
  *
  *  The mapping between the variable names and this peace of documentation is
  *  done next to each variable.
@@ -63,13 +64,13 @@ public:
 
   /**
    * @brief EKF, constructor of the class
-   * @param numerical_jac: do we compute the Jacobians alanytically or not?
+   * @param numerical_jac: do we compute the Jacobians analytically or not?
    * @param is_discrete: is the process discrete?
-   * @param dt: the sampling period of the EKF
+   * @param dt: the sampling period of the EKF.
    */
   EKF(bool numerical_jac, bool is_discrete, double dt, unsigned lin_order=2);
 
-  /** default destructor */
+  /** @brief Default destructor. */
   ~EKF(){}
 
   static const int N = S::state_dim; /*!< State dimension */
@@ -78,7 +79,7 @@ public:
 
 
   /**
-   * @brief updateFilter: compute a pass of the extended kalman filter
+   * @brief updateFilter: compute a pass of the extended Kalman filter
    * @param update_on: is the measurement new?
    *
    * From here on we only have access to \f$ \mu_{t-1} \f$ and
@@ -97,7 +98,7 @@ public:
    * \f}
    * With \f$ E_t \f$ being the jacobian of the noise. In [2], \f$ E_t \f$ is
    * also considered as the control jacobian in a way.
-   * - 2/ Obtain the matrix M expoential
+   * - 2/ Obtain the matrix M exponential
    * \f{align}
    *  e^{M \Delta t} =
    *  \left[ \left.
@@ -111,7 +112,7 @@ public:
    * \f{align}
    *  R_{t, disc} = e^{G_t \Delta t} = (e^{G_t^T \Delta t})^T
    * \f}
-   * - 4/ Compute the discretized process jacbian as follow:
+   * - 4/ Compute the discretized process jacobian as follow:
    * \f{align}
    *  F_{t, disc} = R_{t, disc} \;\;\; e^{M \Delta t}[\text{upper right corner}]
    * \f}
@@ -129,7 +130,7 @@ public:
    *    \tilde{\Sigma}_{t} &= G_t \Sigma_{t-1} G_t^T + R_t
    * \f}
    * The second step consist in introducing the actual measurement when
-   * one occure.
+   * one occur.
    * \f{align}
    *    K_{t} &= \tilde{\Sigma}_{t} H_t^T (H_t \tilde{\Sigma}_t H_t^T + Q_t)^{-1} \\
    *    \mu_{t} &= \tilde{\mu}_{t} + K_t (z_t - h(\tilde{\mu}_t)) \\
@@ -221,26 +222,41 @@ public:
    */
   bool check_jacobians(bool evaluate);
 
-  Eigen::MatrixXd VL_mat_;      /*!< for Van Loan discretization, \f$ M \f$ */
-  Eigen::MatrixXd exp_VL_mat_;  /*!< for Van Loan discretization, \f$ e^{M dt} \f$ */
+  /*!< for Van Loan discretization, \f$ M \f$ */
+  Eigen::MatrixXd VL_mat_;
+  /*!< for Van Loan discretization, \f$ e^{M dt} \f$ */
+  Eigen::MatrixXd exp_VL_mat_;
 
-  Eigen::MatrixXd proc_jac_disc_;     /*!< discretized/discrete process Jacobian, \f$ G_{t,disc}/G_t \f$ */
-  Eigen::MatrixXd proc_jac_;          /*!< user input process Jacobian, \f$ G_t \f$ */
-  Eigen::MatrixXd noise_jac_;         /*!< Jacobian of the process noise, \f$ E_t \f$ */
-  Eigen::MatrixXd meas_jac_;          /*!< Jacobian of the measurement, \f$ H_t \f$ */
-  Eigen::MatrixXd proc_noise_;        /*!< user input covariance of the process noise \f$ R_t \f$ */
-  Eigen::MatrixXd proc_noise_disc_;   /*!< discretized/discrete covariance of the process noise \f$ R_{t,disc}/R_t \f$ */
-  Eigen::MatrixXd meas_noise_;        /*!< user input covariance of the measure noise \f$ Q_t \f$ */
-  Eigen::MatrixXd meas_noise_disc_;   /*!< covariance of the process noise \f$ Q_t \f$ assuming a discrete measurement */
-  Eigen::MatrixXd gain_mat_;          /*!< Kalman filter gain matrix \f$ K_t \f$ */
+  /*!< discretized/discrete process Jacobian, \f$ G_{t,disc}/G_t \f$ */
+  Eigen::MatrixXd proc_jac_disc_;
+  /*!< user input process Jacobian, \f$ G_t \f$ */
+  Eigen::MatrixXd proc_jac_;
+  /*!< Jacobian of the process noise, \f$ E_t \f$ */
+  Eigen::MatrixXd noise_jac_;
+  /*!< Jacobian of the measurement, \f$ H_t \f$ */
+  Eigen::MatrixXd meas_jac_;
+  /*!< user input covariance of the process noise \f$ R_t \f$ */
+  Eigen::MatrixXd proc_noise_;
+  /*!< discretized/discrete covariance of the process noise \f$ R_{t,disc}/R_t \f$ */
+  Eigen::MatrixXd proc_noise_disc_;
+  /*!< user input covariance of the measure noise \f$ Q_t \f$ */
+  Eigen::MatrixXd meas_noise_;
+  /*!< covariance of the process noise \f$ Q_t \f$ assuming a discrete measurement */
+  Eigen::MatrixXd meas_noise_disc_;
+  /*!< Kalman filter gain matrix \f$ K_t \f$ */
+  Eigen::MatrixXd gain_mat_;
 
-  Eigen::LDLT<Eigen::MatrixXd> ldlt;  /*!< LDLT decomposition to invert some matrix */
+  /*!< LDLT decomposition to invert some matrix */
+  Eigen::LDLT<Eigen::MatrixXd> ldlt;
 
-  bool numerical_jac_;                /*!< Do we compute the Jacobian
-                                           numericaly or analyticaly? */
-  bool is_discrete_;                  /*!< Is the process discrete? */
-  double dt_;                         /*!< sampling period */
-  unsigned lin_order_;                     /*!< order of the process dynamic linearization */
+  /*!< Do we compute the Jacobian numerically or analytically? */
+  bool numerical_jac_;
+  /*!< Is the process discrete? */
+  bool is_discrete_;
+  /*!< sampling period */
+  double dt_;
+  /*!< order of the process dynamic linearization */
+  unsigned lin_order_;
 };
 
 #include "mim_estimation/filtering_tools/ekf.hxx"
