1fd1d4f2abb7dced1eccf089fe3efa1a5626dde4
shahramkhorshidi <khorshidishahram@gmail.com>
2021-07-15 16:55:03 +0200

    modeify ekf_test on vicon data

--- demos/ekf_test.py
+++ demos/ekf_test.py
@@ -1,14 +1,18 @@
+"""This module runs the EKF on the data obtained by vicon from solo12 motion. 
+Download the data from 'rsec-motion-group' page, in 'ReactiveStepping', Squatting and Wobbling motions for solo12.
+Modify the 'path' variable to the path where the data is located.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 from mim_estimation.ekf import EKF
 import mim_estimation.conf as conf
 import pinocchio as pin
+from math import pi
 
 
 def read_data(file_name):
     data = np.loadtxt(file_name)
     return data[:T, 1:]
-
 
 def plot(x, y, x_label, y_label, title):
     t = np.arange(T)
@@ -21,19 +25,17 @@ def plot(x, y, x_label, y_label, title):
         plt.grid()
     plt.legend(loc="upper right", shadow=True, fontsize="large")
     plt.xlabel("time(ms)")
-    plt.suptitle(title)
-    plt.show()    
+    plt.suptitle(title) 
 
 def run_ekf(path):
     # Read the data from robot motion
-    folder = "/home/skhorshidi/Documents/files/"
-    imu_lin_acc = read_data(folder + path + "dg_solo12-imu_accelerometer.dat")
-    imu_ang_vel = read_data(folder + path + "dg_solo12-imu_gyroscope.dat")
-    joint_positions = read_data(folder + path + "dg_solo12-joint_positions.dat")
-    joint_velocities = read_data(folder + path + "dg_solo12-joint_velocities.dat")
-    base_position = read_data(folder + path + "dg_vicon_entity-solo12_position.dat")
+    imu_lin_acc = read_data(path + "dg_solo12-imu_accelerometer.dat")
+    imu_ang_vel = read_data(path + "dg_solo12-imu_gyroscope.dat")
+    joint_positions = read_data(path + "dg_solo12-joint_positions.dat")
+    joint_velocities = read_data(path + "dg_solo12-joint_velocities.dat")
+    base_position = read_data(path + "dg_vicon_entity-solo12_position.dat")
     base_velocity_body = read_data(
-        folder + path + "dg_vicon_entity-solo12_velocity_body.dat"
+        path + "dg_vicon_entity-solo12_velocity_body.dat"
     )
 
     # Initialize vectors for data collecting
@@ -64,28 +66,26 @@ def run_ekf(path):
         base_pos_ekf[i, :] = base_state_post.get("ekf_frame_position")
         base_vel_ekf[i, :] = base_state_post.get("ekf_frame_velocity")
         q_ekf = base_state_post.get("ekf_frame_orientation")
-        base_rpy_ekf[i, :] = pin.utils.matrixToRpy(q_ekf.matrix())
+        base_rpy_ekf[i, :] = pin.utils.matrixToRpy(q_ekf.matrix()) * (180/pi)
         
         # Read the value of orientation from the robot
         q_base = pin.Quaternion(base_position[i, 3:])
-        base_rpy[i, :] = pin.utils.matrixToRpy(q_base.matrix())
+        base_rpy[i, :] = pin.utils.matrixToRpy(q_base.matrix()) * (180/pi)
 
     # Plot the results
+    plt.figure("Position")
     plot(base_position, base_pos_ekf, "Vicon", "EKF", "Base_Position")
+
+    plt.figure("Velocity")
     plot(base_velocity_body, base_vel_ekf, "Vicon", "EKF", "Base_Velocity")
-    plot(base_rpy, base_rpy_ekf, "Vicon", "EKF", "Base_Orientation(roll-pitch-yaw)")
+
+    plt.figure("Orientation")
+    plot(base_rpy, base_rpy_ekf, "Vicon", "EKF", "Base_Orientation(roll-pitch-yaw)_degree")
+
+    plt.show()
 
 
 if __name__ == "__main__":
-    # path = "files_squatting/"
-    # path = "files_first_wobbling/"
-    path = "files_second_wobbling/"
-
-    if path == "files_squatting/":
-        T = 24000
-    elif path == "files_first_wobbling/":
-        T = 35000
-    elif path == "files_second_wobbling/":
-        T = 33000
-
+    path = "/home/Documents/files/files_first_wobbling/"
+    T = 35000
     run_ekf(path)
