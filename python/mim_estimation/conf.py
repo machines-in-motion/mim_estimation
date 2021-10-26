88301b0522291039fb3334df73e477bfd9ba7eca
shahramkhorshidi <khorshidishahram@gmail.com>
2021-10-26 11:16:15 +0200

    correct the covarince matrices

--- python/mim_estimation/conf.py
+++ python/mim_estimation/conf.py
@@ -18,10 +18,10 @@ SE3_imu_to_base = pin.SE3(np.identity(3), np.array([0.10407, -0.00635, 0.01540])
 
 # noise covariances (TO-BE-TUNED)
 # prediction noise
-eta_a = (0.00078 ** 2) * np.ones([3])
-eta_omega = (0.000523 ** 2) * np.ones([3])
+eta_a = (0.0001962 ** 2) * np.ones([3])
+eta_omega = (0.0000873 ** 2) * np.ones([3])
 eta_b_a = (0.0001 ** 2) * np.ones([3])
-eta_b_omega = (0.000618 ** 2) * np.ones([3])
+eta_b_omega = (0.000309 ** 2) * np.ones([3])
 
 Q_a = np.diag(eta_a)
 Q_omega = np.diag(eta_omega)
@@ -30,4 +30,4 @@ Qb_omega = np.diag(eta_b_omega)
 
 # measurement noise
 R = np.zeros((12, 12), dtype=float)
-np.fill_diagonal(R, np.array([1e-5, 1e-5, 1e-8]))
+np.fill_diagonal(R, np.array([1e-4, 1e-4, 1e-4]))
